628742c4bb2f19655773c8c13c032646891fca25
klangner <klangner@gmail.com>
2017-06-16 23:32:01 +0200

    Full scraping for ustawy. Save to csv

--- build.sbt
+++ build.sbt
@@ -4,4 +4,7 @@ version := "1.0"
 
 scalaVersion := "2.12.1"
 
-libraryDependencies += "net.ruippeixotog" %% "scala-scraper" % "2.0.0-RC2"
+libraryDependencies ++= Seq(
+  "net.ruippeixotog" %% "scala-scraper" % "2.0.0-RC2",
+  "com.github.tototoshi" %% "scala-csv" % "1.3.4"
+)
--- src/main/scala/pl/klangner/scraper/DziennikUstawScraper.scala
+++ src/main/scala/pl/klangner/scraper/DziennikUstawScraper.scala
@@ -1,9 +1,11 @@
 package pl.klangner.scraper
 
+import java.io.File
+
+import com.github.tototoshi.csv.CSVWriter
 import net.ruippeixotog.scalascraper.browser.JsoupBrowser
-import net.ruippeixotog.scalascraper.dsl.DSL._
 import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
-import net.ruippeixotog.scalascraper.dsl.DSL.Parse._
+import net.ruippeixotog.scalascraper.dsl.DSL._
 
 
 /**Pobieranie dzienników ustaw */
@@ -12,29 +14,55 @@ object DziennikUstawScraper {
   val SITE_URL = "http://isap.sejm.gov.pl/"
   val browser = JsoupBrowser()
 
+  case class DziennikUstaw(id: String, url: String, pdf: Option[String])
 
   def scrap(): Unit = {
+    val xs = scrapAllYear()
+      .flatMap(scrapYear)
+      .flatMap(scrapSection)
+      .map(scrapDetails)
+    save(xs)
+  }
+
+  def scrapAllYear(): Seq[String] = {
     val doc = browser.get(SITE_URL + "VolumeServlet?type=wdu")
-    val yearNodes = doc >> elementList(".cel_black11")
-    val links = yearNodes.map(node => node >> attr("href"))
-                         .filter(_.contains("rok"))
-                         .flatMap(scrapYear)
-                         .flatMap(scrapSection)
-    links.take(10).foreach(println)
+    (doc >> elementList(".cel_black11"))
+      .map(node => node >> attr("href"))
+      .filter(_.contains("rok"))
   }
 
   /** Return links for all sections in given year */
   def scrapYear(link: String): Seq[String] = {
     val doc = browser.get(SITE_URL + link)
-    val sectionLinks = doc >> elementList(".cel_black11")
-    sectionLinks.map(node => node >> attr("href")).filter(_.contains("rok"))
+    (doc >> elementList(".cel_black11"))
+      .map(node => node >> attr("href"))
+      .filter(_.contains("rok"))
   }
 
   /** Return list of links for a given section */
   def scrapSection(link: String): Seq[String] = {
     val doc = browser.get(SITE_URL + link)
-    val sectionLinks = doc >> elementList("a")
-    sectionLinks.map(node => (node >?> attr("href")).getOrElse(""))
+    (doc >> elementList("a"))
+      .map(node => (node >?> attr("href")).getOrElse(""))
       .filter(_.contains("DetailsServlet?id="))
+  }
+
+  def scrapDetails(link: String): DziennikUstaw = {
+    val id = link.substring(link.indexOf("id=")+3)
+    val docUrl = SITE_URL + link
+    val doc = browser.get(docUrl)
+    val pdfLink = (doc >> elementList("a"))
+      .map(node => (node >?> attr("href")).getOrElse(""))
+      .find(_.contains("Download"))
+      .map(l => SITE_URL + l.replace("&amp;", "&").substring(1))
+
+    DziennikUstaw(id, docUrl, pdfLink)
+  }
+
+  def save(xs: Seq[DziennikUstaw]): Unit = {
+    val writer = CSVWriter.open(new File("data/ustawy.csv"))
+    writer.writeRow(List("id", "pdf"))
+    xs.foreach(x => writer.writeRow(List(x.id, x.pdf.getOrElse(""))))
+    writer.close()
   }
 }
